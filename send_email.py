47307fec4a80b91cffe23acb636dcc7d98997f4f
Rohit20ek <49610045+Rohit20ek@users.noreply.github.com>
2024-01-23 15:49:31 +0000

    updated

--- send_email.py
+++ send_email.py
@@ -11,7 +11,7 @@ Subject: Github Email Report
 
 this is your daily email
 """
-context = ss.create_default_context()
+context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,USER_PASSWORD)
     server.sendmail(USERNAME,USERNAME,message) #from,to,message
